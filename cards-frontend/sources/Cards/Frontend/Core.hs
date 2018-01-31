b300ccce5ff83b57c8bfb868deb4cd651b293fad
Spiros Boosalis <SamBoosalis@gmail.com>
2018-01-31 12:05:09 -0800

    onClick, specialized `domEvent Click`

--- cards-frontend/sources/Cards/Frontend/Core.hs
+++ cards-frontend/sources/Cards/Frontend/Core.hs
@@ -28,6 +28,213 @@ import Data.Text (Text)
 -- import Data.Monoid
 -- import System.Environment
 
+{-NOTES
+
+create an HTML element, with (1) Dynamic attributes and a (2) child element, returning its Events:
+
+    elDynAttr'
+     :: Text
+     -> Dynamic (Map Text Text)
+     -> m a 
+     -> m (El, a)
+
+elDynAttr' ~ elDynAttr' NoXMLNamespace :
+
+    elDynAttr'
+     = elDynAttrNS' Nothing
+
+elDynAttr ~ snd <&> elDynAttr' :
+
+    elDynAttr tag dAttributes child
+     = snd <$> elDynAttr' tag dAttributes child
+
+elDynClass ~ ("class" =:) <$> elDynAttr :
+
+    elDynClass tag dClass
+     = elDynAttr tag (("class" =:) <$> dClass)
+
+elAttr ~ pure >>> elDynAttr :
+
+    elAttr tag attributes child
+     = elDynAttr tag (pure attributes) child
+    
+
+type El = Element EventResult GhcjsDomSpace
+
+
+
+module Reflex.Dom.Widget.Basic
+
+button :: DomBuilder t m => Text -> m (Event t ())
+button t = do
+  (e, _) <- element "button" def $ text t
+  return $ domEvent Click e
+
+i.e.
+
+button :: (... t m) => Text -> m (Event t ())
+button t = do
+  (e, _) <- element "button" def $ text t
+  let eClick = domEvent Click e 
+  return eClick
+
+button' :: MonadWidget t m => Text -> m ( t ())
+button' t = do
+  (esButton, _) <- element "button" def $ text t
+  -- let eClick = domEvent Click esButton
+  return esButton
+
+
+
+  Abort :: EventName 'AbortTag
+  Blur :: EventName 'BlurTag
+  Change :: EventName 'ChangeTag
+  Click :: EventName 'ClickTag
+  Contextmenu :: EventName 'ContextmenuTag
+  Dblclick :: EventName 'DblclickTag
+  Drag :: EventName 'DragTag
+  Dragend :: EventName 'DragendTag
+  Dragenter :: EventName 'DragenterTag
+  Dragleave :: EventName 'DragleaveTag
+  Dragover :: EventName 'DragoverTag
+  Dragstart :: EventName 'DragstartTag
+  Drop :: EventName 'DropTag
+  Error :: EventName 'ErrorTag
+  Focus :: EventName 'FocusTag
+  Input :: EventName 'InputTag
+  Invalid :: EventName 'InvalidTag
+  Keydown :: EventName 'KeydownTag
+  Keypress :: EventName 'KeypressTag
+  Keyup :: EventName 'KeyupTag
+  Load :: EventName 'LoadTag
+  Mousedown :: EventName 'MousedownTag
+  Mouseenter :: EventName 'MouseenterTag
+  Mouseleave :: EventName 'MouseleaveTag
+  Mousemove :: EventName 'MousemoveTag
+  Mouseout :: EventName 'MouseoutTag
+  Mouseover :: EventName 'MouseoverTag
+  Mouseup :: EventName 'MouseupTag
+  Mousewheel :: EventName 'MousewheelTag
+  Scroll :: EventName 'ScrollTag
+  Select :: EventName 'SelectTag
+  Submit :: EventName 'SubmitTag
+  Wheel :: EventName 'WheelTag
+  Beforecut :: EventName 'BeforecutTag
+  Cut :: EventName 'CutTag
+  Beforecopy :: EventName 'BeforecopyTag
+  Copy :: EventName 'CopyTag
+  Beforepaste :: EventName 'BeforepasteTag
+  Paste :: EventName 'PasteTag
+  Reset :: EventName 'ResetTag
+  Search :: EventName 'SearchTag
+  Selectstart :: EventName 'SelectstartTag
+  Touchstart :: EventName 'TouchstartTag
+  Touchmove :: EventName 'TouchmoveTag
+  Touchend :: EventName 'TouchendTag
+  Touchcancel :: EventName 'TouchcancelTag
+
+
+{- |
+
+>>> KeyCode 32 -- the <enter> key
+
+-}
+newtype KeyCode = KeyCode
+ { getKeyCode :: Word
+ }
+
+{- |
+
+>>> ScrollDistance (-30) -- 30 "ticks" down?
+
+-}
+newtype ScrollDistance = ScrollDistance
+ { getScrollDistance :: Double
+ }
+
+{- |
+
+>>> MousePosition x y
+
+-}
+data MousePosition = MousePosition
+ { getHorizontalPosition :: Int
+ , getVerticalPosition   :: Int
+ }
+
+{-|
+
+@
+-- module Reflex.Dom.Builder.Class.Events
+data TouchEventResult = TouchEventResult
+ { _touchEventResult_altKey         :: Bool
+ , _touchEventResult_ctrlKey        :: Bool
+ , _touchEventResult_metaKey        :: Bool
+ , _touchEventResult_shiftKey       :: Bool
+ , _touchEventResult_changedTouches :: [TouchResult]
+ , _touchEventResult_targetTouches  :: [TouchResult]
+ , _touchEventResult_touches        :: [TouchResult]
+ }
+@
+
+-}
+type Touch = TouchEventResult
+
+EventResultType 'KeypressTag = Word
+  EventResultType 'KeydownTag = Word
+  EventResultType 'KeyupTag = Word
+
+EventResultType 'ScrollTag = Double
+
+  EventResultType 'DblclickTag = (Int, Int)
+  EventResultType 'MousemoveTag = (Int, Int)
+  EventResultType 'MousedownTag = (Int, Int)
+  EventResultType 'MouseupTag = (Int, Int)
+
+EventResultType 'TouchstartTag = TouchEventResult
+  EventResultType 'TouchmoveTag = TouchEventResult
+  EventResultType 'TouchendTag = TouchEventResult
+  EventResultType 'TouchcancelTag = TouchEventResult
+
+EventResultType 'ClickTag = ()
+  EventResultType 'MouseenterTag = ()
+  EventResultType 'MouseleaveTag = ()
+  EventResultType 'FocusTag = ()
+  EventResultType 'BlurTag = ()
+  EventResultType 'ChangeTag = ()
+  EventResultType 'DragTag = ()
+  EventResultType 'DragendTag = ()
+  EventResultType 'DragenterTag = ()
+  EventResultType 'DragleaveTag = ()
+  EventResultType 'DragoverTag = ()
+  EventResultType 'DragstartTag = ()
+  EventResultType 'DropTag = ()
+  EventResultType 'AbortTag = ()
+  EventResultType 'ContextmenuTag = ()
+  EventResultType 'ErrorTag = ()
+  EventResultType 'InputTag = ()
+  EventResultType 'InvalidTag = ()
+  EventResultType 'LoadTag = ()
+  EventResultType 'MouseoutTag = ()
+  EventResultType 'MouseoverTag = ()
+  EventResultType 'MousewheelTag = ()
+  EventResultType 'SelectTag = ()
+  EventResultType 'SubmitTag = ()
+  EventResultType 'BeforecutTag = ()
+  EventResultType 'CutTag = ()
+  EventResultType 'BeforecopyTag = ()
+  EventResultType 'CopyTag = ()
+  EventResultType 'BeforepasteTag = ()
+  EventResultType 'PasteTag = ()
+  EventResultType 'ResetTag = ()
+  EventResultType 'SearchTag = ()
+  EventResultType 'SelectstartTag = ()
+  EventResultType 'WheelTag = ()
+
+
+
+-}
+
 ----------------------------------------
 
 {-|
@@ -132,15 +339,22 @@ runQuery db q = db & filter (\Card{..} -> q `T.isInfixOf` _cardName)
 formatResults :: (MonadWidget t m) => Results -> m ()
 formatResults
  = traverse_ formatCard
-
  -- = fmap formatCard
- -- > intersperse (sequence_ [div_, text "========================================", div_])
+ -- > intersperse (sequence_
+ --                [div_, text "========================================", div_])
  -- > sequence_
 
+cssDisplayInline :: Map Text Text
+cssDisplayInline = "display" =: "inline"
+
 formatCard :: (MonadWidget t m) => Card -> m ()
-formatCard Card{..} = divClass "card" $ do
- div $ text _cardName
- div $ text _cardText
+formatCard Card{..} = elDynAttr "div" (pure cssDisplayInline) $ do
+  div $ text _cardName
+  div $ text _cardText
+
+ -- divClass "card-result" $ do  
+ -- div $ text _cardName
+ -- div $ text _cardText
 
 -- formatResults :: Results -> Text
 -- formatResults
@@ -160,6 +374,31 @@ defaultCardDatabase = fmap (Card & uncurry)
  --                        , _textInputConfig_setValue = never
  --                        , _textInputConfig_attributes = constDyn mempty }
 
+
+{- QuickRef
+
+-- Simplest form.  Create a widget of given type containing the given child.
+-- Return whatever the child returns.
+[W]   el         :: Text ->                                  m a -> m a
+
+-- This version returns the 'El' as well.
+[W]   el'        :: Text ->                                  m a -> m (El, a)
+
+-- These two additionally apply attributes to the element, such as ("class" =: "blah")
+[W]   elAttr     :: Text ->            Map Text Text ->      m a -> m a
+[W]   elAttr'    :: Text ->            Map Text Text ->      m a -> m (El, a)
+
+-- As above, but now the attribute map is Dynamic
+[W]   elDynAttr  :: Text ->   Dynamic (Map Text Text) ->     m a -> m a
+[W]   elDynAttr' :: Text ->   Dynamic (Map Text Text) ->     m a -> m (El, a)
+
+-- As above, but with an optional XML namespace for the tag.  Note that this does *not* set the 'xmlns' attribute.  See https://www.w3.org/TR/DOM-Level-2-Core/core.html#ID-DocCrElNS
+[W]   elDynAttrNS' :: Maybe Text -> Text -> Dynamic (Map Text Text) -> m a -> m (El, a)
+
+-- Shortcut for elAttr when you only want to set the "class" attribute.
+[W]   elClass    :: Text ->                       Text ->    m a -> m a
+
+-}
 
 {-
 
--- cards-frontend/sources/Cards/Frontend/Extra.hs
+++ cards-frontend/sources/Cards/Frontend/Extra.hs
@@ -3,6 +3,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLists #-}
 
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds, ConstraintKinds #-}
+
 {-|
 
 -}
@@ -15,6 +18,99 @@ import Reflex.Dom
 
 import Prelude.Spiros hiding (Text,div)
 
+---------------------------------------
+
+{-|
+
+@
+> mainWidget :: SomeWidget () -> IO ()
+@
+
+-}
+type SomeWidget a = (forall x. Widget x a)
+
+{-|
+
+@
+>>> :i Widget
+-- module Reflex.Dom.Main
+type Widget x =
+  PostBuildT
+    DomTimeline
+    (ImmediateDomBuilderT
+       DomTimeline
+       (WithJSContextSingleton
+          x
+          (PerformEventT DomTimeline DomHost)))
+  :: *
+  -> *
+
+instance Monad (Widget x)
+
+>>> :i Widget_
+-- module Cards.Frontend.Extra
+type Widget_ x =
+  PostBuildT DomTimeline
+    (ImmediateDomBuilderT DomTimeline
+       (WithJSContextSingleton x
+          (PerformEventT DomTimeline
+             DomHost
+             ())))
+             
+  :: *
+
+type DomTimeline = Spider
+
+type role WithJSContextSingleton phantom representational nominal
+
+@
+
+-}
+type Widget_ x = Widget x ()
+
+----------------------------------------
+
+{-| 
+
+NOTES
+
+@
+> :i El
+type El = Element EventResult GhcjsDomSpace :: * -> *
+@
+
+so
+
+@
+El ~ El' GhcjsDomSpace
+@
+
+-}  
+type El' d = Element EventResult d
+
+----------------------------------------
+
+{-| (has kind @'EventTag' -> Constraint@). 
+
+NOTES
+
+@
+@
+
+-}  
+type HasEvent t d event =
+  HasDomEvent t (El' d t) event
+
+{-| (has kind @Constraint@). 
+
+NOTES
+
+@
+@
+
+-}  
+type HasClick t d = HasEvent t d 'ClickTag
+
 ----------------------------------------
 
 -- | @<div>...</div>@
@@ -25,3 +121,107 @@ div = el "div"
 div_ :: (MonadWidget t m) => m ()
 div_ = div blank
 
+----------------------------------------
+
+
+{-|
+
+@
+domEvent <name>   :: <element> -> Event <type>
+domEvent <name> _ ::              Event <type>
+@
+
+
+@
+Click :: EventName 'ClickTag
+domEvent _ :: Event 
+@
+
+@
+
+domEvent Click
+  :: HasDomEvent t target 'ClickTag
+  => target
+  -> Event t (DomEventType target 'ClickTag)
+
+domEvent Click blank
+  :: (Monad m, HasDomEvent t (m ()) 'ClickTag)
+  => Event t (DomEventType (m ()) 'ClickTag)
+
+domEvent Click
+  :: HasDomEvent t target 'ClickTag
+  => target
+  -> Event t (DomEventType target 'ClickTag)
+
+@
+
+NOTES
+
+@
+-- `Element` is the most general element
+-- (versus the specialized TextInput, InputElement, TextAreaElement, etc).
+
+instance forall k t (d :: k) (en :: EventTag).
+ Reflex t =>
+ HasDomEvent t (Element EventResult d t) en
+
+data Element (er :: EventTag -> *) (d :: k) t
+ = Element
+  { _element_events :: EventSelector t
+                      (Data.Functor.Misc.WrapArg er EventName)
+  , _element_raw    :: RawElement d
+  }
+
+@
+
+-}
+--onClick :: DomBuilder t m =>  -> m (Event t ())
+onClick
+  :: (HasDomEvent t target 'ClickTag)
+  => target
+  -> Event t (DomEventType target 'ClickTag)
+onClick = domEvent Click
+
+{-| specialized 'onClick'.
+
+@
+= 'domEvent' 'Click'
+@
+
+NOTES
+
+@
+> :i El
+type El = Element EventResult GhcjsDomSpace :: * -> *
+
+> :i DomEventType
+type instance DomEventType (Element EventResult d t) en
+  = EventResultType en
+
+> :i EventResultType
+type family EventResultType (en :: EventTag) :: *  where
+ EventResultType 'ClickTag = ()
+ ...
+
+@
+
+so
+
+@
+DomEventType (El' d t) 'ClickTag
+~
+DomEventType (Element EventResult d t) 'ClickTag
+~
+EventResultType 'ClickTag
+~
+()
+@
+
+-}  
+onClick'
+  :: (HasClick t d)
+  => (El' d t)
+  -> Event t ()
+onClick' = domEvent Click
+
+----------------------------------------
