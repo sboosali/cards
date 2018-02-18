a65451a83e1a455489a2e2ee5c25e6e9f9f29912
Spiros Boosalis <SamBoosalis@gmail.com>
2018-02-18 01:19:55 -0800

    copy `CardData` from magic-card-search

--- cards-common/sources/MTGJSON/AllSets/Types.hs
+++ cards-common/sources/MTGJSON/AllSets/Types.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveAnyClass #-}
+--{-# LANGUAGE DataKinds #-}
+
 {-|
 
 -}
@@ -7,6 +10,373 @@ import MTGJSON.Extra
 
 ----------------------------------------
 
+-- data Card = Card
+--  {
+--  }
 
+----------------------------------------
+
+{-| a more richly-typed 'CardObject'. i.e. parsed, simplified, and validated.
+
+documentation for the fields is below.  
+
+* @'_CardData_id'@: 
+
+gameplay-relevant stuff, card characteristics: 
+
+* @'_CardData_name'@: 
+* @'_CardData_manaCost'@: 
+* @'_CardData_colors'@: 
+* @'_CardData_type'@: 
+* @'_CardData_text'@: 
+
+quasi-derivable stuff, card characteristics: 
+
+* @'_CardData_cmc'@: 
+* @'_CardData_colorIdentity'@: 
+* @'_CardData_names'@: 
+* @'_CardData_type'@: 
+
+not actually derivable, without some special casing, because of color markers. e.g. Pact Of Negation has a blue dot on the left of the type line (with accompanying reminder text for people with visual impairments), which makes it blue as a characteristic. 
+
+non-gameplay-relevant stuff, card characteristics: 
+
+* @'_CardData_layout'@: 
+* @'_CardData_watermark'@: 
+* @'_CardData_rarity'@: 
+* @'_CardData_flavor'@: 
+* @'_CardData_artist'@: 
+* @'_CardData_number'@: 
+
+Internet data: 
+
+* @'_CardData_multiverseid'@: 
+* @'_CardData_mciNumber'@: 
+
+links the card to other resources, like images or results from from the two standard magic card search engines (below): 
+
+data for other printings: 
+ 
+* @'_CardData_variations'@: 
+* @'_CardData_printings'@: 
+* @'_CardData_originalText'@: 
+* @'_CardData_originalType'@: 
+* @'_CardData_foreignNames'@: 
+
+miscellaneous data:  
+
+* @'_CardData_rulings'@: 
+* @'_CardData_legalities'@: 
+
+-}
+data CardData = CardData 
+  { _CardData_id            :: CardId 
+
+  -- gameplay-relevant stuff, card characteristic 
+  , _CardData_name          :: CardName 
+  , _CardData_manaCost      :: Maybe ManaCost 
+  , _CardData_colors        :: [CardColor] 
+  , _CardData_type          :: CardTypeLine 
+  , _CardData_text          :: CardText 
+
+  -- quasi-derivable stuff, card characteristics 
+  , _CardData_cmc           :: ConvertedManaCost 
+  , _CardData_colorIdentity :: [CardColorIdentity] 
+  , _CardData_names         :: Maybe [CardName] 
+  , _CardData_supertypes    :: [CardSupertype] 
+  , _CardData_types         :: (NonEmpty CardTypes) 
+  , _CardData_subtypes      :: [CardSubtype] 
+
+  -- non-gameplay-relevant stuff, card characteristics 
+  , _CardData_layout        :: CardLayout 
+  , _CardData_watermark     :: Maybe Text 
+  , _CardData_rarity        :: KnownCardRarity 
+  , _CardData_flavor        :: CardFlavorText 
+  , _CardData_artist        :: CardArtist
+  , _CardData_number        :: CardCollectorNumber 
+
+  -- other resources
+  , _CardData_multiverseid  :: WizardsIdentifier 
+  , _CardData_mciNumber     :: CardCollectorNumber -- ^ used by `MagicCards.info`, almost always identical to '_CardData_number'
+
+  , _CardData_rulings       :: [CardRuling] 
+  , _CardData_legalities    :: [CardFormatLegality]
+
+  , _CardData_variations    :: [WizardsIdentifier] 
+  , _CardData_printings     :: [CardSetCode] 
+  , _CardData_originalText  :: Maybe CardText 
+  , _CardData_originalType  :: Maybe CardTypeLine 
+  , _CardData_foreignNames  :: [CardForeignPrinting] 
+  } deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-
+
+-- stuff I don't care about, though the Vanguard-only characteristics below can be moved up to the type-dependent-characteristics above 
+, _CardData_starter       :: Bool -- IsCardStarter
+, _CardData_source        :: Maybe Text
+, _CardData_imageName     :: Maybe Text
+, _CardData_releaseDate   :: Maybe CardReleaseDate -- ^ Promo only
+, _CardData_life          :: Maybe VanguardLifeModifier -- ^ Vanguard only
+, _CardData_hand          :: Maybe VanguardHandModifier -- ^ Vanguard only
+
+-- set specific stuff: old sets, time spiral, and UN-sets 
+, _CardData_reserved      :: Bool -- IsCardReserved
+, _CardData_timeshifted   :: Bool -- IsCardTimeShifted
+, _CardData_border        :: Maybe CardBorderColor
+
+-}
+
+{-| the number on the right of some cards. 
+
+nothing has either power or toughness without having both. 
+
+nothing with a power/toughness has a loyalty.
+
+-}
+data CardCharacteristicNumber 
+  = CardCharacteristicPowerToughness CardNumber CardNumber 
+  | CardCharacteristicLoyalty CardNumber 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| for power, toughness, loyalty. 
+
+-}
+data CardNumber 
+  = CardIntegerNumber  Integer -- ^ the printed number, the most frequent case. can be negative: e.g. Char-Rumbler, which has a power of @'CardIntegerNumber' -1@. (Un-cards can have non-integer power/toughness, which we're ignoring)
+  | CardWildcardNumber Integer -- ^ the integer represents the modifier: @1@ is @\*+1@, @0@ is just @\*@. e.g. Tarmogoyf has a power of  @'CardWildcardNumber' 0@ and a toughness of @'CardWildcardNumber' 1@. 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable) 
+
+----------------------------------------
+
+{-| unifies the following, with invariants:  
+
+* '_CardObject_supertypes'
+* '_CardObject_types'
+* '_CardObject_subtypes'
+
+-}
+data CardTypes = CardTypes 
+  { _CardTypes_supertypes :: [CardSupertype] 
+  , _CardTypes_types      :: NonEmpty CardType 
+  , _CardTypes_subtypes   :: [CardSubtype] 
+  } deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| most non-creature cards have a single card type, without supertypes or subtypes. 
+
+-}
+defaultCardTypes :: CardType -> CardTypes
+defaultCardTypes t = CardTypes{..} 
+  where 
+  _CardTypes_supertypes = []
+  _CardTypes_types      = t :| [] 
+  _CardTypes_subtypes   = [] 
+
+-- TODO pair the subtype to the card type , except with the tribal supertype , which provides creature subtypes to non-creature cartoons 
+{-| 
+
+-}
+data CardSupertype
+  = KnownCardSupertype KnownCardSupertype 
+  | UnknownCardSupertype Text 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data CardType
+ = KnownCardType KnownCardType 
+ | UnknownCardType Text 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data CardSubtype 
+  = KnownCardSubtype KnownCardSubtype 
+  | UnknownCardSubtype Text 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+----------------------------------------
+
+{-| 
+
+-}
+newtype ConvertedManaCost = ConvertedManaCost Natural 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| the unique card identifier. 
+
+for each set in @AllSets.json@, every card has an identifier that distinct from every other card, including the same cardname in other sets.
+
+-}
+data CardId = CardId Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardName = CardName Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data ManaCost = ManaCost Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-|  
+
+-}
+data CardColor = CardColor Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardTypeLine = CardTypeLine Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardText = CardText Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable) 
+
+{-| 
+
+-}
+data CardColorIdentity = CardColorIdentity Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardWholeName = CardWholeName Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardLayout = CardLayout Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardWatermark = CardWatermark Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+----------------------------------------
+
+{-| 
+
+-}
+data KnownCardRarity  
+  = Common 
+  | Uncommon 
+  | Rare 
+  | Mythic 
+  | Timeshifted -- TODO
+  deriving (Show,Read,Eq,Ord,Enum,Bounded,Generic,Data,NFData,Hashable)
+  
+{-| 
+
+-}
+newtype CardFlavorText = CardFlavorText Text 
+ deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable) 
+
+{-| 
+
+-}
+newtype CardArtist = CardArtist Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| 
+
+-}
+data CardCollectorNumber = CardCollectorNumber Text 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+{-| the Multiverse ID, used by `gather.wizards.com`. 
+
+-}
+data WizardsIdentifier = WizardsIdentifier Text 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| used by `MagicCards.info`, almost always identical to '_CardData_number' 
+
+-}
+data MagicCardsInfoIdentifier = MagicCardsInfoIdentifier Text 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data CardSetCode = CardSetCode Text 
+  deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+----------------------------------------
+
+{-| 
+
+-}
+data CardForeignPrinting = CardForeignPrinting 
+  { _CardForeignPrinting_language     :: KnownLanguage 
+  , _CardForeignPrinting_name         :: CardName 
+  , _CardForeignPrinting_multiverseid :: WizardsIdentifier 
+  } deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data CardRuling = CardRuling 
+  { _CardRuling_date :: Text -- TODO Day needs a hashable instance 
+  , _CardRuling_text :: Text 
+  } deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data CardFormatLegality = CardFormatLegality 
+  { _CardFormatLegality_format   :: KnownMagicFormat 
+  , _CardFormatLegality_legality :: KnownMagicLegality 
+  } deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data KnownMagicFormat = KnownMagicFormat Text -- TODO 
+  deriving (Show,Read,Eq,Ord,Data,Generic,NFData,Hashable)
+
+----------------------------------------
+
+{-| 
+
+-}
+data KnownMagicLegality 
+  = Legal 
+  | Restricted 
+  | Banned 
+  deriving (Show,Read,Eq,Ord,Enum,Bounded,Generic,Data,NFData,Hashable)
+
+{-| 
+
+-}
+data KnownLanguage
+    = LanguageEN
+    | LanguageES 
+    deriving (Show,Read,Eq,Ord,Enum,Bounded,Generic,Data,NFData,Hashable)
+
+data KnownCardSupertype 
+    = TribalSupertype 
+    | SnowSupertype 
+    deriving (Show,Read,Eq,Ord,Enum,Bounded,Generic,Data,NFData,Hashable)
+
+data KnownCardType 
+    = InstantType 
+    | SorceryType 
+    deriving (Show,Read,Eq,Ord,Enum,Bounded,Generic,Data,NFData,Hashable)
+
+type KnownCardSubtype = Text -- TODO 
 
 ----------------------------------------
